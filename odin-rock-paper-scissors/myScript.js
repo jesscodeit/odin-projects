e757a0a0df166fad1ffc5fd8462bff6110b5bbda
Jessika <jesscodeit@gmail.com>
2021-10-11 16:44:18 -0500

    created new folder html and js files for the project rock paper scissors. added an outline of tasks to js file.

---
+++ odin-rock-paper-scissors/myScript.js
@@ -0,0 +1,10 @@
+// make a function that will randomly return: rock, paper or scissors
+
+// prompt the user to choose: rock, paper or scissors
+// ensure user selection is case-insensitive 
+
+// create a function that plays a single round of RPS
+// have it return strings like "scissors beats paper, you lose."
+
+// create a function that plays a five round game of RPS
+// have it keep score and report winner/loser at the end
