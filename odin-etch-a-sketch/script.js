e0db466c80e56dc0fba6ecc1e53bb24a61c647ef
Jessika <jesscodeit@gmail.com>
2021-12-08 15:48:37 -0600

    after much mental stress, figured out what was going wrong with problems stated in last commit. time to celebrate! And then I will go after the optional functionalities.

--- odin-etch-a-sketch/script.js
+++ odin-etch-a-sketch/script.js
@@ -1,14 +1,15 @@
 //creates a single pixel block for the grid
 function createPixels() {
-    let makePixel = document.createElement('div');
-    let classMe = document.createAttribute('class');
-    classMe.value = "pixel noPaint";
-    makePixel.setAttributeNode(classMe);
-    document.getElementById('pixelContainer').appendChild(makePixel);
+    let makeDiv = document.createElement('div');
+    let makeClass = document.createAttribute('class');
+    makeClass.value = "pixel";
+    makeDiv.setAttributeNode(makeClass);
+    document.getElementById('sketchBoard').appendChild(makeDiv);
 }
 
 //create the board, grid initially set to 16 by 16 blocks
 let boardWidth = 16;
+
 function buildBoard() {
     for (let i = 0; i < (boardWidth ** 2); i++) {
         createPixels();
@@ -16,49 +17,61 @@ function buildBoard() {
 }
 buildBoard();
 
-//remove all pixel divs
 function destroyBoard() {
-    let board = document.getElementById('pixelContainer');
+    let board = document.getElementById('sketchBoard');
     while (board.firstChild) {
         board.removeChild(board.firstChild);
     }
 }
 
-//on mouseover, change the color of the pixel block
-let pixels = document.querySelectorAll('.pixel');
-function paintBrush() {
-    pixels.forEach(function (i) {
-    i.addEventListener('mouseover', function() {
-        i.classList.add('paint');
-        });
-    });
-}
-paintBrush();
-
-//removes paint class from board pixels
-let reset = document.querySelector('button');
-reset.addEventListener('click', () => {
-    pixels.forEach(function(element) {
-        element.classList.remove('paint');
-    });
-    promptForNum();
-});
-
-// prompt user for new number
 let userNum;
 function promptForNum() {
     userNum = prompt("How many pixels wide should the new board be? \nThe maximum width is 100.", "42");
     console.log(userNum);
+}
+
+function checkUserNum() {
     if (isNaN(userNum)) {
         alert("That is not a number! Oh well, here is the same board.");
+        userNum = 16;
+    }
+    else if (userNum == null) {
+        alert("Did you mean to press that button? Oh well, here is the same board.");
+        userNum = 16;
+    }
+    else if (userNum > 100) {
+        alert("Wow, that's a bit much. How about we just make it 100 by 100.");
+        userNum = 100;
+        boardWidth = userNum;
     }
     else {
         alert(`Okay! Let's make it ${userNum} pixels wide!`);
         boardWidth = userNum;
-        destroyBoard();
-        buildBoard();
-        paintBrush();
-        //need to figure out why mouseover painting doesn't work after board reset...
-        //also need to figure out style to keep container same size no matter block count.
     }
 }
+
+function resetBoardSize() {
+    document.getElementById("sketchBoard").style.gridTemplateColumns = `repeat(${userNum}, 1fr)`;
+    document.getElementById("sketchBoard").style.gridTemplateRows = `repeat(${userNum}, 1fr)`;
+}
+
+function clickReset() {
+    destroyBoard();
+    promptForNum();
+    checkUserNum();
+    resetBoardSize();
+    buildBoard();
+    makeItPaintable();
+}
+
+document.getElementById("resetButton").addEventListener('click', clickReset);
+
+//on mouseover, change the color of the "pixel" div
+function makeItPaintable() {
+    document.querySelectorAll('.pixel').forEach(function(e) {
+    e.addEventListener('mouseover', function() {
+        e.classList.add('painted');
+    });
+});
+}
+makeItPaintable();
